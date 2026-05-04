41d25fbe97bfe99f211459d23c1a59d3f0e1a32a
isaac <isaacloff@gmail.com>
2026-05-04 14:00:00 +0100

    stored_system_metadata can be tested

--- src/stored_system/stored_system_metadata/mod.rs
+++ src/stored_system/stored_system_metadata/mod.rs
@@ -1,5 +1,6 @@
 pub mod system_criteria;
 
+use aion_event::prelude::CurrentEvents;
 use aion_program::prelude::{ResourceId, ValuePassword, UserId, UserPassword};
 
 use crate::prelude::{SystemCriteria};
@@ -8,5 +9,22 @@ pub struct StoredSystemMetadata {
     criteria: SystemCriteria,
     resource_id: ResourceId,
     program_password: Option<ValuePassword>,
-    system_details: Option<(UserId, UserPassword)>
+    system_details: Option<(UserId, UserPassword)>,
+
+    // will also check the system at runtime
+    readonly: bool,
+}
+
+impl StoredSystemMetadata {
+    pub fn resource_id(&self) -> &ResourceId {
+        &self.resource_id
+    }
+
+    pub fn is_readonly(&self) -> bool {
+        self.readonly
+    }
+
+    pub fn test_events(&self, events: &CurrentEvents) -> bool {
+        self.criteria.test(events)
+    }
 }
