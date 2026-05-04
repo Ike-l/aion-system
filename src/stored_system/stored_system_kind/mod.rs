08c573c0d8e82e4e637a87cc675c3784bfcf6999
isaac <isaacloff@gmail.com>
2026-05-04 21:24:49 +0100

    removed sync primitives fro stored_system_kind

--- src/stored_system/stored_system_kind/mod.rs
+++ src/stored_system/stored_system_kind/mod.rs
@@ -1,8 +1,6 @@
-use std::sync::{Arc, Mutex};
-
 use crate::prelude::{StoredAsyncSystem, StoredSyncSystem};
 
 pub enum StoredSystemKind {
-    Sync(Arc<Mutex<StoredSyncSystem>>),
-    Async(Arc<Mutex<StoredAsyncSystem>>)
+    Sync(StoredSyncSystem),
+    Async(StoredAsyncSystem)
 }
