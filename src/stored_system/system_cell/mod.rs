f307bfdd54d25d6a205ab68e8de06535ff9e4b1b
isaac <isaacloff@gmail.com>
2026-05-04 19:43:04 +0100

    added to system cell

--- src/stored_system/system_cell/mod.rs
+++ src/stored_system/system_cell/mod.rs
@@ -10,4 +10,29 @@ impl SystemCell {
     pub fn new(system: StoredSystemKind) -> Self {
         Self { system: UnsafeCell::new(system) }
     }
+
+    pub fn consume(self) -> StoredSystemKind {
+        self.system.into_inner()
+    }
+
+    /// # Safety
+    /// 
+    /// Ensure Aliasing Rules are not violated
+    pub unsafe fn get(&self) -> &mut StoredSystemKind {
+        unsafe { &mut *self.system.get() }
+    }
 }
+
+/// # Safety
+/// 
+/// Functions are always Send
+/// 
+/// -Closures aren't 
+unsafe impl Send for SystemCell {}
+
+/// # Safety
+/// 
+/// Functions are always Sync
+/// 
+/// -Closures aren't 
+unsafe impl Sync for SystemCell {}
