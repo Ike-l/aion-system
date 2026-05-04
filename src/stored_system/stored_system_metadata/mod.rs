c5fe65edbd78af7c3a1657e37c8388b05a8cf322
isaac <isaacloff@gmail.com>
2026-05-04 10:11:12 +0100

    metadata holds the resource id of the system

--- src/stored_system/stored_system_metadata/mod.rs
+++ src/stored_system/stored_system_metadata/mod.rs
@@ -1,7 +1,10 @@
 pub mod system_criteria;
 
+use aion_program::prelude::ResourceId;
+
 use crate::prelude::{SystemCriteria};
 
 pub struct StoredSystemMetadata {
     criteria: SystemCriteria,
+    resource_id: ResourceId
 }
