4ec4408b3604f365d531d57d1cf20780223feda8
isaac <isaacloff@gmail.com>
2026-05-04 10:58:10 +0100

    system metadata holds all needed info for execute

--- src/stored_system/stored_system_metadata/mod.rs
+++ src/stored_system/stored_system_metadata/mod.rs
@@ -1,10 +1,12 @@
 pub mod system_criteria;
 
-use aion_program::prelude::ResourceId;
+use aion_program::prelude::{ResourceId, ValuePassword, UserId, UserPassword};
 
 use crate::prelude::{SystemCriteria};
 
 pub struct StoredSystemMetadata {
     criteria: SystemCriteria,
-    resource_id: ResourceId
+    resource_id: ResourceId,
+    program_password: Option<ValuePassword>,
+    system_details: Option<(UserId, UserPassword)>
 }
