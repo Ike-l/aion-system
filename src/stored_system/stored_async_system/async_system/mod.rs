2c6101114d0f91d654ac41aca1a037e5682fbed1
isaac <isaacloff@gmail.com>
2026-05-04 11:39:00 +0100

    async system

--- src/stored_system/stored_async_system/async_system/mod.rs
+++ src/stored_system/stored_async_system/async_system/mod.rs
@@ -1,1 +1,17 @@
+use std::{pin::Pin, sync::Arc};
+
+use crate::prelude::{SystemResult, SystemError};
+
+use aion_program::prelude::{ProgramRegistry, ProgramId, ValuePassword, UserId, UserPassword};
+
 pub mod into_async_system;
+
+pub trait AsyncSystem: Send + Sync {
+    fn execute<'a>(
+        &'a mut self,
+        program_registry: &Arc<ProgramRegistry>,
+        program_id: &ProgramId,
+        program_password: Option<&ValuePassword>,
+        user_details: Option<(&UserId, &UserPassword)>
+    ) -> Pin<Box<dyn Future<Output = Result<Option<SystemResult>, SystemError>> + 'a + Send>>;
+}
