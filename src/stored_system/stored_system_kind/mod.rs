80362bbe4195bffa03b5190568cb3020123b0f09
isaac <isaacloff@gmail.com>
2026-05-06 22:54:45 +0100

    stored system kind builders

--- src/stored_system/stored_system_kind/mod.rs
+++ src/stored_system/stored_system_kind/mod.rs
@@ -1,6 +1,16 @@
-use crate::prelude::{StoredAsyncSystem, StoredSyncSystem};
+use crate::prelude::{StoredAsyncSystem, StoredSyncSystem, IntoAsyncSystem, IntoSyncSystem, SyncSystem, AsyncSystem};
 
 pub enum StoredSystemKind {
     Sync(StoredSyncSystem),
     Async(StoredAsyncSystem)
+}
+
+impl StoredSystemKind {
+    pub fn new_sync<T, S, I>(system: T) -> Self where T: IntoSyncSystem<I, System = S>, S: SyncSystem + 'static {
+        Self::Sync(Box::new(system.into_system()))
+    }
+
+    pub fn new_async<T, S, I>(system: T) -> Self where T: IntoAsyncSystem<I, System = S>, S: AsyncSystem + 'static {
+        Self::Async(Box::new(system.into_system()))
+    }
 }
