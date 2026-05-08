ddad0287bd5c728c41c8e52e444b91ad6ea47e50
isaac <isaacloff@gmail.com>
2026-05-08 23:58:30 +0100

    stored system functions

--- src/stored_system/mod.rs
+++ src/stored_system/mod.rs
@@ -1,3 +1,6 @@
+use std::sync::Arc;
+
+use aion_program::prelude::AccessBuilder;
 pub use aion_program::prelude::{ProgramRegistry};
 
 use crate::prelude::{StoredSystemKind};
@@ -11,6 +14,34 @@ pub mod system_error;
 pub mod function_system_base;
 
 pub struct StoredSystem {
-    pub kind: Option<StoredSystemKind>,
-    pub enabled: bool
+    kind: Option<StoredSystemKind>,
+    enabled: bool
+}
+
+impl StoredSystem {
+    pub fn take_system(&mut self) -> Option<StoredSystemKind> {
+        self.kind.take()
+    }
+
+    pub fn put_system(&mut self, system: StoredSystemKind) -> Option<StoredSystemKind> {
+        self.kind.replace(system)
+    }
+
+    pub fn can_run(
+        &self, 
+        program_registry: &Arc<ProgramRegistry>,
+        auto_access_builder: &AccessBuilder,
+        manual_access_builders: Vec<&AccessBuilder>
+    ) -> bool {
+        if !self.enabled {
+            return false;
+        }
+
+        match &self.kind {
+            Some(stored_system) => {
+                stored_system.check_access(program_registry, auto_access_builder, manual_access_builders)
+            },
+            None => false,
+        }
+    }
 }
